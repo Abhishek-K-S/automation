7bb4d886bdbe6bfa02f5f710fa59cd8f44ba62bb
Abhishek-K-S <abhiiks27052001@gmail.com>
2023-11-16 19:45:07 +0530

    Default view for unknown devices

--- AutoM/src/screens/DefaultDeviceView.tsx
+++ AutoM/src/screens/DefaultDeviceView.tsx
@@ -1,11 +1,34 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { PropsWithChildren } from 'react'
+import useSocketRequestGenerator from '../utils/SocketRequestGenerator'
+import useSocketListenToInfo from '../utils/SocketListenToInfo';
+import { socketEmit } from '../utils/socket';
+import { socketEndpoints } from '../shared/constants';
+import CustomView from '../components/CustomView';
+import CustomText from '../components/CustomText';
 
-const DefaultDeviceView = () => {
+const DefaultDeviceView = ({device}: PropsWithChildren<{device: string}>) => {
+  const socketRequestGenerator = useSocketRequestGenerator(device);
+  const [socketInfo, lastUpdate] = useSocketListenToInfo<any>(device, {time: 0})
+
+  React.useEffect(()=>{
+    if(!device) return;
+    getDeviceInfo();
+  }, [])
+
+  const getDeviceInfo = ()=>{
+    socketEmit(socketRequestGenerator(socketEndpoints.getInfo));
+  }
   return (
-    <View>
-      <Text>DefaultDeviceView</Text>
-    </View>
+    <CustomView>
+      {Object.keys(socketInfo).map(key=>{
+        return (<CustomView>
+          <CustomText medium>{key}</CustomText>
+          <CustomText>{socketInfo[key]}</CustomText>
+        </CustomView>)
+
+      })}
+    </CustomView>
   )
 }
 
