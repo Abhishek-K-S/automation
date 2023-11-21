ea814620f206df948416315d91df2fe59e422fd6
Abhishek-K-S <abhiiks27052001@gmail.com>
2023-11-21 12:46:28 +0530

    simplified server logic

--- outboundServer/index.ts
+++ outboundServer/index.ts
@@ -1,23 +1,13 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-
-import *  as grpc from '@grpc/grpc-js';
-import {io} from 'socket.io-client'
+import { io } from 'socket.io-client'
 import { socketEvents } from './src/shared/constants';
-import { pumpDataTransferService } from './src/shared/gRPC/pumpDataTransfer_grpc_pb'
-import { grpcDataTransferHandlers } from './src/grpcDataTransfer';
-import { localSocketIOEmitter, socketRequestHandler } from './src/socketRequestHandler';
+import { socketRequestHandler } from './src/socketRequestHandler';
 
-
-
-const port = process.env.PORT || '6999'
 const serverURL = process.env.SERVER_URL || ''
 const domainName = process.env.DOMAIN || 'QU'
 
-console.log('domain name is ', domainName)
-
-//socket logic
 const socket = io(serverURL)
 
 socket.on('connect', ()=>{
@@ -34,22 +24,37 @@ function replyToUser(msg: any): void{
 }
 
 
-//grpc logic
-const grpcServer = new grpc.Server()
-grpcServer.addService(pumpDataTransferService, grpcDataTransferHandlers)
+// //grpc logic
+// const grpcServer = new grpc.Server()
+// grpcServer.addService(pumpDataTransferService, grpcDataTransferHandlers)
 
-localSocketIOEmitter.on(socketEvents.relayMessageToUser, replyToUser)
+import connect from './src/database/connect';
+import { sendToUser } from './src/utils/socketEmitter';
+connect();
+const DeviceHelper = require('./src/Devices/DeviceHandler');
+
+console.log('type of ', typeof DeviceHelper.deviceOffline)
+require('./src/mqtt/mqttServer');
+
+
+sendToUser.on(socketEvents.relayMessageToUser, replyToUser);
 
 
 
 //grcp server listening
-grpcServer.bindAsync(`127.0.0.1:${port}`, grpc.ServerCredentials.createInsecure(), (err, reservedPort)=>{
-    if(err){
-        console.log('could not start the server due to ', err);
-        return;
-    }
-    console.log('listening on port ', reservedPort);
-    grpcServer.start();
-    grpcServer.register
-})
+// grpcServer.bindAsync(`127.0.0.1:${port}`, grpc.ServerCredentials.createInsecure(), (err, reservedPort)=>{
+//     if(err){
+//         console.log('could not start the server due to ', err);
+//         return;
+//     }
+//     console.log('listening on port ', reservedPort);
+//     grpcServer.start();
+//     grpcServer.register
+// })
+
+const loopForever = () => { 
+    setTimeout(loopForever, 3000)
+}
+
+loopForever();
 
