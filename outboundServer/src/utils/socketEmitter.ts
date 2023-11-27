62a949b3e4d799df9b965ba74f4ea2e589213d75
Abhishek-K-S <abhiiks27052001@gmail.com>
2023-11-27 17:37:14 +0530

    Naming conventions

--- outboundServer/src/utils/socketEmitter.ts
+++ outboundServer/src/utils/socketEmitter.ts
@@ -1,12 +1,12 @@
 import { EventEmitter } from "stream";
 import { WithoutAuth, socketEvents } from "../shared/constants";
 
-export const sendToUser = new EventEmitter();
+export const localMessageEmitter = new EventEmitter();
 
 const domainName = process.env.DOMAIN || 'QU'
 
 export const replyWith = (data:any)=>{
-    sendToUser.emit(socketEvents.relayMessageToUser, data)
+    localMessageEmitter.emit(socketEvents.relayMessageToUser, data)
 }
 
 export const sendDataToUser = (endPoint: string, payload: any, opt: {to?: string, targetDevice?: string}) =>{
@@ -20,6 +20,6 @@ export const sendDataToUser = (endPoint: string, payload: any, opt: {to?: string
     replyWith(dataToSend);
 }
 
-export const localErrorEmitter = (type: string, message: string, opt: {to?: string, targetDevice?: string}) => {
+export const sendErrorToUser = (type: string, message: string, opt: {to?: string, targetDevice?: string}) => {
     replyWith({error: true, type, message, senderId: opt.to, targetDevice: opt.targetDevice})
 }
